3cdd6f12dcbf36751cc3e18ccfb6f1f23de9a395
redex <alen@redek.me>
2018-09-20 14:31:30 +0200

    Added wp-config.php - foreach through constants

---
+++ wp-config.php
@@ -0,0 +1,15 @@
+<?php
+
+$table_prefix  = getenv('TABLE_PREFIX') ?: 'wp_';
+
+foreach ($_ENV as $key => $value) {
+  $capitalized = strtoupper($key);
+  if (!defined($capitalized)) {
+    define($capitalized, $value);
+  }
+}
+
+if (!defined('ABSPATH'))
+    define('ABSPATH', dirname(__FILE__) . '/');
+
+require_once(ABSPATH . 'wp-settings.php');
